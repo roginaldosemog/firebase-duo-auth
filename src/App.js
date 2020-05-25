8d9a56c5772cc64f16651a6c03cba4a0b6ea063f
Igor Aragão <roginaldosemog@gmail.com>
2020-05-25 17:03:10 -0300

    Added SignUp and Login pages and private route system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./config/Auth";
+import PrivateRoute from "./config/PrivateRoute";
 
+import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import app from "../config/firebase";
+import app from "../config/Firebase";
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <button onClick={() => app.auth().signOut()}>Sign out</button>
     </>
   );
 };
