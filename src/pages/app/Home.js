4af366e6e2a007a13dc3db0ca6f8d472b030ec44
Igor Aragão <roginaldosemog@gmail.com>
2020-05-29 16:10:06 -0300

    Added logged user info at home and home2

--- src/pages/app/Home.js
+++ src/pages/app/Home.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../config/Auth";
 import { app } from "../../config/Firebase";
 
 const Home = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <>
       <h1>Home Primary</h1>
+      <p>Welcome, {currentUser?.email}</p>
       <button onClick={() => app.auth().signOut()}>Sign out</button>
     </>
   );
--- src/pages/app2/Home2.js
+++ src/pages/app2/Home2.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { app2 } from "../../config/Firebase";
+import { AuthContext } from "../../config/Auth";
 
 const Home2 = () => {
+  const { currentUser2 } = useContext(AuthContext);
   return (
     <>
       <h1>Home Secondary</h1>
+      <p>Welcome, {currentUser2?.email}</p>
       <button onClick={() => app2.auth().signOut()}>Sign out</button>
     </>
   );
